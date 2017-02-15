60ffce2614b41d57610d6f727e8d63306c7c71cf
Jonathan A <iotku@iotku.pw>
2017-02-15 02:47:32 -0800

    Switch back to ffmpeg, copy album art without converting hopefully.

--- flac2320.sh
+++ flac2320.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-parallel-moreutils -i -j$(nproc) sox {} -G -C 320.2 {}.mp3 rate -v -L 44100 dither -- ./*.flac
-#parallel-moreutils -i -j$(nproc) ffmpeg -i {} -codec:a libmp3lame -compression_level 0 -b:a 320k {}.mp3 -- ./*.flac
+DIR="${PWD##*/}[320]"
+mkdir "$DIR"
+#parallel-moreutils -i -j$(nproc) sox {} -G -C 320.2 "320/{}.mp3" rate -v -L 44100 dither -- ./*.flac
+parallel-moreutils -i -j$(nproc) ffmpeg -i {} -codec:a libmp3lame -compression_level 0 -b:a 320k -vcodec copy {}.mp3 -- ./*.flac
 rename .flac.mp3 .mp3 ./*.mp3
+mv *.mp3 "$DIR"
--- flac2v0.sh
+++ flac2v0.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+DIR="${PWD##*/}[V0]"
+mkdir "$DIR"
 
-parallel-moreutils -i -j$(nproc) sox {} -G -C -0.2 {}.mp3 rate -v -L 44100 dither -- ./*.flac
-#parallel-moreutils -i -j$(nproc) ffmpeg -i {} -codec:a libmp3lame -compression_level 0 -qscale:a 0 -ar 44100hz -sample_fmt s16 {}.mp3 -- ./*.flac
+#parallel-moreutils -i -j$(nproc) sox {} -G -C -0.2 {}.mp3 rate -v -L 44100 dither -- ./*.flac
+parallel-moreutils -i -j$(nproc) ffmpeg -i {} -codec:a libmp3lame -compression_level 0 -qscale:a 0 -vcodec copy {}.mp3 -- ./*.flac
 rename .flac.mp3 .mp3 ./*.mp3
+mv *.mp3 "$DIR"
+
